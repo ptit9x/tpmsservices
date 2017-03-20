ac27aa6164549791517d7219a9ff53d6f8d9c294
huynh do ngoc <ngochuynh1991@gmail.com>
2017-03-20 22:01:28 +0700

    Build my framework

--- dist/models/facets.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-// our example model is just an Array
-var facets = [1, 2, 3, 4];
-exports.default = facets;
-//# sourceMappingURL=facets.js.map
