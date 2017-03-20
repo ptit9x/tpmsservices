6c9854f5bf3443b789269375a01aa37b701b4685
huynh.dn <huynh.dn@temando.vn>
2017-03-20 15:47:45 +0700

    add es5 code

---
+++ dist/models/facets.js
@@ -0,0 +1,9 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+// our example model is just an Array
+var facets = [1, 2, 3, 4];
+exports.default = facets;
+//# sourceMappingURL=facets.js.map
